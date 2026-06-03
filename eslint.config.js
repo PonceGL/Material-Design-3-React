b913f57e96555cb24a5cb66ffbaa53d5ebbb03ce
PonceGL <poncianogl@hotmail.com>
2026-06-03 17:19:50 -0600

    chore(eslint): integrate eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
   js.configs.recommended,
@@ -22,5 +23,6 @@ export default tseslint.config(
         { allowConstantExport: true }
       ]
     }
-  }
+  },
+  prettierConfig
 );
