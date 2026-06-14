ef787e2781ad397ec414eee4cf7836b7bcdac70d
PonceGL <poncianogl@hotmail.com>
2026-06-14 16:20:11 -0600

    feat(RCL-118): implement SplitButton compound component
    SplitButton.Root + SplitButton.Leading + SplitButton.Trailing,
    compuestos vía Object.assign. Leading reutiliza .md3-button (size m);
    Trailing es un botón cuadrado de 48x48dp. Trailing soporta uso como
    toggle (checked/onCheckedChange) reflejado en aria-pressed y
    data-checked.

---
+++ packages/material-design-3/src/components/Buttons/SplitButton/index.ts
@@ -0,0 +1,7 @@
+export { SplitButton } from './SplitButton';
+export type {
+  SplitButtonLeadingProps,
+  SplitButtonProps,
+  SplitButtonShapes,
+  SplitButtonTrailingProps,
+} from './SplitButton.types';
